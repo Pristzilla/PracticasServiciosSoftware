f0542dcdba861c5cd82084c412556a0446a00c84
Javier <javier.barquine@gmail.com>
2022-02-25 18:12:40 +0100

    cosas hechas

--- Practica2Proveedor/src/main/java/es/unican/ss/volumenArea/VolumenAreaImpl.java
+++ Practica2Proveedor/src/main/java/es/unican/ss/volumenArea/VolumenAreaImpl.java
@@ -14,39 +14,39 @@ import javax.xml.ws.BindingType;
 @WebService(portName = "SOAPPort", serviceName = "VolumenArea", targetNamespace = "http://www.unican.es/ss/VolumenArea/", wsdlLocation = "http://localhost:8080/VolumenArea?wsdl", endpointInterface = "es.unican.ss.volumenArea.VolumenAreaInterface")
 @BindingType("http://schemas.xmlsoap.org/wsdl/soap/http")
 public class VolumenAreaImpl
-    implements VolumenAreaInterface
+implements VolumenAreaInterface
 {
 
 
-    public VolumenAreaImpl() {
-    }
+	public VolumenAreaImpl() {
+	}
 
-    /**
-     * Retorna el area del objeto dado
-     * 
-     * @param params
-     * @return
-     *     returns double
-     * @throws DatosNoValidosException
-     */
-    public double area(Dimensiones params)
-        throws DatosNoValidosException
-    {
-        return params.getAlto()*params.getLargo();
-    }
+	/**
+	 * Retorna el area del objeto dado
+	 * 
+	 * @param params
+	 * @return
+	 *     returns double
+	 * @throws DatosNoValidosException
+	 */
+	public double area(Dimensiones params)
+			throws DatosNoValidosException
+	{
+		return params.getAlto() * params.getLargo();
+	}
 
-    /**
-     * Retorna el volumen del objeto dado 
-     * 
-     * @param params
-     * @return
-     *     returns double
-     * @throws DatosNoValidosException
-     */
-    public double volumen(Dimensiones params)
-        throws DatosNoValidosException
-    {
-        return params.getAlto()*params.getAncho()*params.getLargo();
-    }
+	/**
+	 * Retorna el volumen del objeto dado 
+	 * 
+	 * @param params
+	 * @return
+	 *     returns double
+	 * @throws DatosNoValidosException
+	 */
+	public double volumen(Dimensiones params)
+			throws DatosNoValidosException
+	{
+		return params.getAlto() * params.getAncho() * params.getLargo();
+	}
 
 }
