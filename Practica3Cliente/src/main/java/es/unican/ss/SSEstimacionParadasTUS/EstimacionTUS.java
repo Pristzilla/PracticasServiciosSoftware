d6282cc8ab6db437a5064e2e8a9f4209487103e5
Sara Grela <sgc114@alumnos.unican.es>
2022-03-11 18:49:53 +0100

    Cliente listo

---
+++ Practica3Cliente/src/main/java/es/unican/ss/SSEstimacionParadasTUS/EstimacionTUS.java
@@ -0,0 +1,114 @@
+
+package es.unican.ss.SSEstimacionParadasTUS;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para estimacionTUS complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="estimacionTUS"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="direccion" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="estimacion1" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="estimacion2" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "estimacionTUS", propOrder = {
+    "direccion",
+    "estimacion1",
+    "estimacion2"
+})
+public class EstimacionTUS {
+
+    protected String direccion;
+    protected String estimacion1;
+    protected String estimacion2;
+
+    /**
+     * Obtiene el valor de la propiedad direccion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * Define el valor de la propiedad direccion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDireccion(String value) {
+        this.direccion = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad estimacion1.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEstimacion1() {
+        return estimacion1;
+    }
+
+    /**
+     * Define el valor de la propiedad estimacion1.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEstimacion1(String value) {
+        this.estimacion1 = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad estimacion2.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEstimacion2() {
+        return estimacion2;
+    }
+
+    /**
+     * Define el valor de la propiedad estimacion2.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEstimacion2(String value) {
+        this.estimacion2 = value;
+    }
+
+}
---
+++ Practica3Cliente/src/main/java/es/unican/ss/SSEstimacionParadasTUS/GetEstimacionSiguienteBusResponse.java
@@ -0,0 +1,62 @@
+
+package es.unican.ss.SSEstimacionParadasTUS;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para getEstimacionSiguienteBusResponse complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="getEstimacionSiguienteBusResponse"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="return" type="{http://unican.es/ss/SSEstimacionParadasTUS}estimacionTUS" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getEstimacionSiguienteBusResponse", propOrder = {
+    "_return"
+})
+public class GetEstimacionSiguienteBusResponse {
+
+    @XmlElement(name = "return")
+    protected EstimacionTUS _return;
+
+    /**
+     * Obtiene el valor de la propiedad return.
+     * 
+     * @return
+     *     possible object is
+     *     {@link EstimacionTUS }
+     *     
+     */
+    public EstimacionTUS getReturn() {
+        return _return;
+    }
+
+    /**
+     * Define el valor de la propiedad return.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link EstimacionTUS }
+     *     
+     */
+    public void setReturn(EstimacionTUS value) {
+        this._return = value;
+    }
+
+}
