d009b545ad99f25c0df299172a65a4997c5fd5e5
Javier <javier.barquine@gmail.com>
2022-03-31 21:33:51 +0200

    Cuando puedas, mete tu ruta absoluta donde hayas guardado el xml en el ProcesaJAXB jeje

--- Practica4Proveedor/src/main/java/ss/unican/ssjornadas/procesaXML/ProcesaJornadasJAXB.java
+++ Practica4Proveedor/src/main/java/ss/unican/ssjornadas/procesaXML/ProcesaJornadasJAXB.java
@@ -10,17 +10,27 @@ import ss.unican.ssjornadas.entidades.PrimeraRFEF;
 
 public class ProcesaJornadasJAXB {
 	
-	public static void main(String[] args) {
+	public PrimeraRFEF procesaFichero(String ordenador) {
 
 		JAXBContext jaxbctx = null;
+		PrimeraRFEF liga = null;
+		String ruta;
+		if(ordenador.equals("sara")) {
+			ruta="/Usuarios/Windows/Sara";
+		}
+		else {
+			ruta = "/Users/barquinj/GitHub/PracticasServiciosSoftware/primeraRFEF.xml";
+		}
 		try {
 			jaxbctx = JAXBContext.newInstance(PrimeraRFEF.class);
 
 			// Se procesa el documento (Unmarshall)
 			Unmarshaller unmarshaller = jaxbctx.createUnmarshaller();
-			PrimeraRFEF liga = (PrimeraRFEF) unmarshaller.unmarshal(new File("src/main/resources/InsuranceElements.xml"));
+			liga = (PrimeraRFEF) unmarshaller.unmarshal(new File(ruta));
+			return liga;
 		}catch(JAXBException e) {
 			e.printStackTrace();
+			return null;
 		}
 
 	}
