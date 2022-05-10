c1545a10a94546629211cb54789e0f496306d18a
Javier <javier.barquine@gmail.com>
2022-05-10 10:34:15 +0200

    creado el controller

---
+++ Practica6Proveedor/src/main/java/es/unican/ss/Practica6Proveedor/controlador/EmpresaSegurosController.java
@@ -0,0 +1,8 @@
+package es.unican.ss.Practica6Proveedor.controlador;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpresaSegurosController {
+
+}
